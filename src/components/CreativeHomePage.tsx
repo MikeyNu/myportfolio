ce9f74070d4ac78e3c41012b8ce37b6295995a55
Michael Ndhlovu <mikeynuking@gmail.com>
2026-08-14 20:14:46 +0200

    preserve video-only landing page

--- src/components/CreativeHomePage.tsx
+++ src/components/CreativeHomePage.tsx
@@ -1,21 +1,9 @@
 import { ModernHero } from './ModernHero';
-import { CreativeIntroBar } from './CreativeIntroBar';
-import { CreativeCapabilitiesGrid } from './CreativeCapabilitiesGrid';
-import { CreativeClientLogoRail } from './CreativeClientLogoRail';
-import { FeaturedCaseStudies } from './FeaturedCaseStudies';
 
-interface CreativeHomePageProps {
-  onViewCaseStudy: (projectId: string) => void;
-}
-
-export function CreativeHomePage({ onViewCaseStudy }: CreativeHomePageProps) {
+export function CreativeHomePage() {
   return (
     <div className="min-h-screen">
       <ModernHero />
-      <CreativeIntroBar />
-      <CreativeCapabilitiesGrid />
-      <FeaturedCaseStudies onViewCaseStudy={onViewCaseStudy} />
-      <CreativeClientLogoRail />
     </div>
   );
 }
